18448a3aab7aa003138d622798cd316e84ed62be
Raven <tomonow.a@yandex.ru>
2023-12-13 14:42:18 +0300

    update

--- conftest.py
+++ conftest.py
@@ -1,31 +1,46 @@
 import pytest
 from selenium import webdriver
 
-from selenium.webdriver.chrome.service import Service as ChromeChrome
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose from langs en/ru/fr/...")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+
     browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
     browser = None
+
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(service=ChromeChrome(ChromeDriverManager().install()))
+        if language != None:
+            print("\nstart chrome browser for test..")
+            options = ChromeOptions()
+            options.add_experimental_option('prefs', {'intl.accept_languages': language})
+            browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+        else:
+            raise pytest.UsageError("--language should be en/ru/fr/...")
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        if language != None:
+            print("\nstart firefox browser for test..")
+            options = FirefoxOptions()
+            options.set_preference("intl.accept_languages", language)
+            browser = webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))
+        else:
+            raise pytest.UsageError("--language should be en/ru/fr/...")
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.maximize_window()
-    # return browser
     yield browser
     print("\nquit browser..")
     browser.quit()
